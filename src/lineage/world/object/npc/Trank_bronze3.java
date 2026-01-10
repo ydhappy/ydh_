008940eae5c19363d9ef88469c513e32c4b42aad
ydhappy <94909904+ydhappy@users.noreply.github.com>
2026-01-10 17:14:49 +0900

    삭제
    삭제

--- src/lineage/world/object/npc/Trank_bronze3.java
+++
@@ -1,7 +0,0 @@
-package lineage.world.object.npc;
-
-import lineage.world.object.object;
-
-public class Trank_bronze3 extends object {
-
-}
--- src/lineage/world/object/npc/background/Torch.java
+++
@@ -1,7 +0,0 @@
-package lineage.world.object.npc.background;
-
-import lineage.world.object.instance.BackgroundInstance;
-
-public class Torch extends BackgroundInstance {
-
-}
