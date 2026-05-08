bd11dc1ea54d1c2e4fb195defdb3fb6624e79944
ydhappy <94909904+ydhappy@users.noreply.github.com>
2026-05-08 18:35:13 +0900

    Add lore tile maps to world map data

--- data/maps.js
+++ data/maps.js
@@ -72,6 +72,90 @@ window.YDH_MAPS = {
         'TSSSSSSSSSSSSSST',
         'TTTTTTTTTTTTTTTT'
       ]
+    },
+    {
+      id: 'black-moon-ruins',
+      name: '검은 달 폐허',
+      description: '달문양 제단이 무너진 자리입니다. 밤마다 그림자가 먼저 움직입니다.',
+      start: { x: 2, y: 9 },
+      portalTo: 4,
+      rows: [
+        'TTTTTTTTTTTTTTTT',
+        'TSSSSSSSMMMMMSPT',
+        'TSTTTSSSTTTTSSST',
+        'TSSRRRSSSSSSSSST',
+        'TSSRTTTTSSMMMSTT',
+        'TSSRNNRSSSSSSSST',
+        'TSSRTTTSSSTTTTST',
+        'TSSRRRRSSSSMMMSP',
+        'TSTTTTRSSSTTTTST',
+        'TSSSSRRRSSSSSSST',
+        'TSSSSSSSSSSSSSST',
+        'TTTTTTTTTTTTTTTT'
+      ]
+    },
+    {
+      id: 'starlight-archive',
+      name: '별빛 기록관',
+      description: '기억과 이름을 보관하는 살아있는 서고입니다. 책장이 미궁처럼 움직입니다.',
+      start: { x: 1, y: 9 },
+      portalTo: 5,
+      rows: [
+        'TTTTTTTTTTTTTTTT',
+        'TSSSSSSSSSSSSSPT',
+        'TSSTTTTSSSTTTTST',
+        'TSSNSSRSSSSMMMSP',
+        'TTTTSSRSSSTTTTST',
+        'TMMMMMRRRRSSSSST',
+        'TSSSTTTSSRSSNNST',
+        'TSSSSSSSSRSSSSST',
+        'TSTTTTSSSRSSMMST',
+        'TRRRRRRRRRSSSSST',
+        'TSSSSSSSSSSSSSST',
+        'TTTTTTTTTTTTTTTT'
+      ]
+    },
+    {
+      id: 'mirror-marsh',
+      name: '거울 늪',
+      description: '수면에 비친 것은 현재가 아니라 선택하지 않은 미래입니다.',
+      start: { x: 2, y: 8 },
+      portalTo: 6,
+      rows: [
+        'TTTTTTTTTTTTTTTT',
+        'TGGGWWWWWGGGMGPT',
+        'TGGRRRRWWGGGGGGT',
+        'TGGWMMRWWTTTGGGT',
+        'TNNWRRRRRGGGMMGT',
+        'TGGWWWWWRGGGGGGT',
+        'TGGGMMGWRGGTTTGT',
+        'TGGTTTGWRGGGGGGT',
+        'TGRRRRRRRGGMMGGT',
+        'TGGGWWWWGGGGGGGT',
+        'TGGGGGGGGGGGGGGT',
+        'TTTTTTTTTTTTTTTT'
+      ]
+    },
+    {
+      id: 'abyss-throne',
+      name: '심연의 왕좌',
+      description: '검은 달의 군주가 잠든 최종 전장입니다. 모든 포탈이 이곳으로 접힙니다.',
+      start: { x: 2, y: 9 },
+      portalTo: 0,
+      rows: [
+        'TTTTTTTTTTTTTTTT',
+        'TSSSSSMMMMSSSSPT',
+        'TSTTTTSSSSTTTTST',
+        'TSSMMSSSSSSMMsst'.toUpperCase(),
+        'TSSSTTTNNRTTTTST',
+        'TMMMMSSRRRSSMMMT',
+        'TSSSTTTSSRTTTTST',
+        'TSSMMSSSSRSSMMST',
+        'TSTTTTSSSRSSSSST',
+        'TRRRRRRRRRSSSSST',
+        'TSSSSSMMMMSSSSST',
+        'TTTTTTTTTTTTTTTT'
+      ]
     }
   ]
 };
